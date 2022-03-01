7858f6cf605468f738267003a27e836f00a7ab2b
macaeco <macarenafernandezayusob@gmail.com>
2022-03-01 19:13:56 +0100

    tortura

--- src/components/filter/index.jsx
+++ src/components/filter/index.jsx
@@ -1,13 +1,25 @@
-import './style.css'
+import './style.css';
+import CardContext from '../../context/cards-context'
+import { useContext } from 'react';
 function BodyFilter(){
+    const [cardsData, , , , , setFilterCard] = useContext(CardContext);
+    const handleOnChange=(e)=> {
+        const selectCard = cardsData.filter(card=>card.title.toLowerCase().includes(e.target.value.toLowerCase()))
+        setFilterCard(selectCard)
+    }
+
+    
+
+
     return(
+
         <body className="filter__container">
             <div className="filter__info">
                 <h5 className='filter__info1'>Version 1.0</h5>
                 <h5 className='filter__info2'>Updated on 12 Apr</h5>
             </div>
             <div className='filter__div__container'>
-                <input type="text" className="filter__filter" placeholder="🔍 Filter Cards"/>
+                <input type="text" className="filter__filter" placeholder="🔍 Filter Cards" onChange={handleOnChange}/>
             </div>
            
 
